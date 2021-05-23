534b25d5e9a088594ed427c7ba4b9ff5204515b6
eyohnarose <56161338+eyohnarose@users.noreply.github.com>
2021-05-23 22:04:27 +0800

    Update sysctl.sh
    Update: sysctl changes

--- sysctl.sh
+++ sysctl.sh
@@ -11,24 +11,6 @@ fi
 sed -i -e "/net.ipv4.ip_forward/d" $SYSCTLCONFIG
 echo "net.ipv4.ip_forward=1" >> $SYSCTLCONFIG
 
-sed -i -e "/net.ipv4.conf.all.accept_redirects/d" $SYSCTLCONFIG
-echo "net.ipv4.conf.all.accept_redirects=0" >> $SYSCTLCONFIG
-
-sed -i -e "/net.ipv4.conf.all.send_redirects/d" $SYSCTLCONFIG
-echo "net.ipv4.conf.all.send_redirects=0" >> $SYSCTLCONFIG
-
-sed -i -e "/net.ipv4.conf.default.rp_filter/d" $SYSCTLCONFIG
-echo "net.ipv4.conf.default.rp_filter=0" >> $SYSCTLCONFIG
-
-sed -i -e "/net.ipv4.conf.default.accept_source_route/d" $SYSCTLCONFIG
-echo "net.ipv4.conf.default.accept_source_route=0" >> $SYSCTLCONFIG
-
-sed -i -e "/net.ipv4.conf.default.send_redirects/d" $SYSCTLCONFIG
-echo "net.ipv4.conf.default.send_redirects=0" >> $SYSCTLCONFIG
-
-sed -i -e "/net.ipv4.icmp_ignore_bogus_error_responses/d" $SYSCTLCONFIG
-echo "net.ipv4.icmp_ignore_bogus_error_responses=1" >> $SYSCTLCONFIG
-
 sysctl -p
 
 cat /etc/sysctl.d/*.conf /etc/sysctl.conf | sysctl -e -p -
